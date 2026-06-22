c84ff5f71f96ef4a84c2aed89f6ce7bf14b5739b
Agenda <crayon0401@gmail.com>
2026-06-23 01:08:26 +0800

    feat: turn taskbar host into control window

--- src/taskbar_host.py
+++ src/taskbar_host.py
@@ -1,28 +1,49 @@
-from PyQt6.QtCore import QEvent, pyqtSignal
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtWidgets import QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QWidget
 
 from src.app_icon import build_app_icon
 
 
 class TaskbarHostWindow(QWidget):
-    activated = pyqtSignal()
-    close_requested = pyqtSignal()
+    toggle_widget_requested = pyqtSignal()
+    quit_requested = pyqtSignal()
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Spotify Lyrics Widget")
         self.setWindowIcon(build_app_icon())
-        self.resize(320, 120)
+        self.resize(320, 140)
+
+        self._title_label = QLabel("Spotify Lyrics Widget")
+        self._status_label = QLabel()
+        self._toggle_button = QPushButton()
+        self._quit_button = QPushButton("Quit")
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self._toggle_button)
+        button_layout.addWidget(self._quit_button)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self._title_label)
+        layout.addWidget(self._status_label)
+        layout.addLayout(button_layout)
+        self.setLayout(layout)
+
+        self._toggle_button.clicked.connect(self.toggle_widget_requested.emit)
+        self._quit_button.clicked.connect(self.quit_requested.emit)
+        self.set_widget_visible(False)
+
+    def set_widget_visible(self, is_visible: bool):
+        if is_visible:
+            self._status_label.setText("Widget: Visible")
+            self._toggle_button.setText("Hide Widget")
+        else:
+            self._status_label.setText("Widget: Hidden")
+            self._toggle_button.setText("Show Widget")
 
     def show_taskbar_entry(self):
         self.showMinimized()
 
-    def changeEvent(self, event):
-        if event.type() == QEvent.Type.WindowStateChange and not self.isMinimized():
-            self.activated.emit()
-            self.showMinimized()
-        super().changeEvent(event)
-
     def closeEvent(self, event):
-        self.close_requested.emit()
-        event.accept()
+        event.ignore()
+        self.showMinimized()
--- tests/test_taskbar_host.py
+++ tests/test_taskbar_host.py
@@ -1,4 +1,4 @@
-from PyQt6.QtCore import QEvent, Qt
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QCloseEvent
 
 from src.taskbar_host import TaskbarHostWindow
@@ -13,7 +13,47 @@ def test_taskbar_host_is_regular_top_level_window(qtbot):
     assert (flags & Qt.WindowType.WindowType_Mask) != Qt.WindowType.Tool
     assert host.windowTitle() == "Spotify Lyrics Widget"
     assert host.width() == 320
-    assert host.height() == 120
+    assert host.height() == 140
+
+
+def test_control_window_starts_with_hidden_widget_state(qtbot):
+    host = TaskbarHostWindow()
+    qtbot.addWidget(host)
+
+    assert host._status_label.text() == "Widget: Hidden"
+    assert host._toggle_button.text() == "Show Widget"
+    assert host._quit_button.text() == "Quit"
+
+
+def test_set_widget_visible_updates_status_and_toggle_button(qtbot):
+    host = TaskbarHostWindow()
+    qtbot.addWidget(host)
+
+    host.set_widget_visible(True)
+
+    assert host._status_label.text() == "Widget: Visible"
+    assert host._toggle_button.text() == "Hide Widget"
+
+    host.set_widget_visible(False)
+
+    assert host._status_label.text() == "Widget: Hidden"
+    assert host._toggle_button.text() == "Show Widget"
+
+
+def test_toggle_button_emits_toggle_widget_requested(qtbot):
+    host = TaskbarHostWindow()
+    qtbot.addWidget(host)
+
+    with qtbot.waitSignal(host.toggle_widget_requested, timeout=1000):
+        host._toggle_button.click()
+
+
+def test_quit_button_emits_quit_requested(qtbot):
+    host = TaskbarHostWindow()
+    qtbot.addWidget(host)
+
+    with qtbot.waitSignal(host.quit_requested, timeout=1000):
+        host._quit_button.click()
 
 
 def test_show_taskbar_entry_minimizes_host(qtbot, monkeypatch):
@@ -27,37 +67,14 @@ def test_show_taskbar_entry_minimizes_host(qtbot, monkeypatch):
     assert calls == ["minimized"]
 
 
-def test_restoring_host_emits_activated_and_minimizes_again(qtbot, monkeypatch):
+def test_close_event_returns_to_taskbar_without_accepting_close(qtbot, monkeypatch):
     host = TaskbarHostWindow()
     qtbot.addWidget(host)
     calls = []
-    monkeypatch.setattr(host, "isMinimized", lambda: False)
     monkeypatch.setattr(host, "showMinimized", lambda: calls.append("minimized"))
-
-    with qtbot.waitSignal(host.activated, timeout=1000):
-        host.changeEvent(QEvent(QEvent.Type.WindowStateChange))
-
-    assert calls == ["minimized"]
-
-
-def test_minimized_state_change_does_not_emit_activated(qtbot, monkeypatch):
-    host = TaskbarHostWindow()
-    qtbot.addWidget(host)
-    signals = []
-    host.activated.connect(lambda: signals.append("activated"))
-    monkeypatch.setattr(host, "isMinimized", lambda: True)
-
-    host.changeEvent(QEvent(QEvent.Type.WindowStateChange))
-
-    assert signals == []
-
-
-def test_close_event_emits_close_requested(qtbot):
-    host = TaskbarHostWindow()
-    qtbot.addWidget(host)
     event = QCloseEvent()
 
-    with qtbot.waitSignal(host.close_requested, timeout=1000):
-        host.closeEvent(event)
+    host.closeEvent(event)
 
-    assert event.isAccepted()
+    assert calls == ["minimized"]
+    assert not event.isAccepted()
